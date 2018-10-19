2a527cf253de4a16200479c18eefe3dbfbbace77
Clement Trouillard <clement.TROUILLARD@canal-plus.com>
2018-10-19 15:31:06 +0200

    Locating the required gigapixels to render...

--- assembler/assemble.c
+++ assembler/assemble.c
@@ -15,27 +15,32 @@
 #include "assemble.h"
 
 static void
-printdim(const char *fname)
+getdim(const char *fname, Dim *dim)
 {
 	VipsImage *file = NULL;
 
 	if (!(file = vips_image_new_from_file(fname, NULL))) {
 		vips_error_exit( NULL );
 	}
-	printf("File: %s - Height:%d Width:%d\n", fname,
+	printf("File: %.40s - Height:%.7d Width:%.7d\n", fname,
 	  vips_image_get_height(file),
 	  vips_image_get_width(file));
+	dim->width += vips_image_get_height(file);
+	dim->height += vips_image_get_width(file);
 	g_object_unref(file);
 }
 
 int
 main(int argc, char *argv[])
 {
+	static Dim dim = {0x0, 0x0};
+
 	if (VIPS_INIT(argv[0])) {
 		vips_error_exit(NULL);
 	}
 	for (ssize_t i = 1; i < argc; i++) {
-		printdim(argv[i]);
+		getdim(argv[i], &dim);
 	}
+	printf("\nTotal Stuff: %lld x %lld\n", dim.height, dim.width);
 	return 0;
 }
--- assembler/assemble.h
+++ assembler/assemble.h
@@ -1,4 +1,9 @@
 #ifndef ASSEMBLE_H
 #define ASSEMBLE_H
 
+typedef struct Sdim {
+	long long height;
+	long long width;
+} Dim;
+
 #endif /* ASSEMBLE_H */
