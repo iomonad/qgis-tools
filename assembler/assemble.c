f9753bd990c96f512ca87f793d5dab210d3be342
Clement Trouillard <clement.TROUILLARD@canal-plus.com>
2018-10-19 15:17:22 +0200

    I transformed a bug into a feature. Once you learn how, you'll never forget it

--- assembler/assemble.c
+++ assembler/assemble.c
@@ -14,11 +14,28 @@
 
 #include "assemble.h"
 
+static void
+printdim(const char *fname)
+{
+	VipsImage *file = NULL;
+
+	if (!(file = vips_image_new_from_file(fname, NULL))) {
+		vips_error_exit( NULL );
+	}
+	printf("File: %s - Height:%d Width:%d\n", fname,
+	  vips_image_get_height(file),
+	  vips_image_get_width(file));
+	g_object_unref(file);
+}
+
 int
 main(int argc, char *argv[])
 {
-	if (VIPS_INIT(argv[0]) || argc < 2) {
+	if (VIPS_INIT(argv[0])) {
 		vips_error_exit(NULL);
 	}
-    return 0;
+	for (ssize_t i = 1; i < argc; i++) {
+		printdim(argv[i]);
+	}
+	return 0;
 }
