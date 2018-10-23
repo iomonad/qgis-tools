bec0d68c047ba4885604a70770f0532bb7dc2a31
Clement Trouillard <clement.TROUILLARD@canal-plus.com>
2018-10-23 14:31:56 +0200

    oops - thought I got that one.

--- assembler/assemble.c
+++ assembler/assemble.c
@@ -21,39 +21,39 @@
 static void
 prepare(Tile *tile, const char * const fname)
 {
-	tile->aref = NULL;
+		tile->aref = NULL;
 
-	if (!(tile->aref = vips_image_new_from_file(fname, NULL))) {
-		vips_error_exit( NULL );
-	}
-	tile->dim.height = vips_image_get_height(tile->aref);
-	tile->dim.width  = vips_image_get_width(tile->aref);
+		if (!(tile->aref = vips_image_new_from_file(fname, NULL))) {
+				vips_error_exit( NULL );
+		}
+		tile->dim.height = vips_image_get_height(tile->aref);
+		tile->dim.width  = vips_image_get_width(tile->aref);
 }
 
 static VipsImage*
 populate(const char *outfile)
 {
-	printf("Writing to outfile: %s\n", outfile);
-	return NULL;
+		printf("Writing to outfile: %s\n", outfile);
+		return NULL;
 }
 
 int
 main(int argc, char *argv[])
 {
-	SLIST_INIT(&head);
+		SLIST_INIT(&head);
 
-	if (VIPS_INIT(argv[0])) {
-		vips_error_exit(NULL);
-	}
-	for (ssize_t i = 1; i < argc; i++) {
-		Tile *tile = NULL;
-
-		if (!(tile = (Tile*)malloc(sizeof(Tile)))) {
-			vips_error_exit( NULL );
+		if (VIPS_INIT(argv[0])) {
+				vips_error_exit(NULL);
 		}
-		prepare(tile, argv[i]);
-		SLIST_INSERT_HEAD(&head, tile, next);
-	}
-	populate(DEFAULT_OUT);
-	return 0;
+		for (ssize_t i = 1; i < argc; i++) {
+				Tile *tile = NULL;
+
+				if (!(tile = (Tile*)malloc(sizeof(Tile)))) {
+						vips_error_exit( NULL );
+				}
+				prepare(tile, argv[i]);
+				SLIST_INSERT_HEAD(&head, tile, next);
+		}
+		populate(DEFAULT_OUT);
+		return 0;
 }
