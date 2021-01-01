a88fdb8f2628b75eff177912399839aeb2db906b
Clement Van Truyart <me@trosa.io>
2021-01-02 00:03:11 +0100

    Web Page WIp

--- docs/script.js
+++ docs/script.js
@@ -1,11 +1,8 @@
-var mapview = L.map('mapid').setView([51.505, -0.09], 13);
+var mapview = L.map('mapid').setView([48.860, 2.333], 14);
 
-
-L.tileLayer('https://api.mapbox.com/styles/v1/{id}/tiles/{z}/{x}/{y}?access_token=pk.eyJ1IjoibWFwYm94IiwiYSI6ImNpejY4NXVycTA2emYycXBndHRqcmZ3N3gifQ.rJcFIG214AriISLbB6B5aw', {
+L.tileLayer('http://basemaps.cartocdn.com/light_all/{z}/{x}/{y}.png', {
     maxZoom: 18,
-    attribution: 'Map data &copy; <a href="https://www.openstreetmap.org/">OpenStreetMap</a> contributors, ' +
-	'<a href="https://creativecommons.org/licenses/by-sa/2.0/">CC-BY-SA</a>, ' +
-	'Imagery © <a href="https://www.mapbox.com/">Mapbox</a>',
+    attribution: '',
     id: 'mapbox/streets-v11',
     tileSize: 512,
     zoomOffset: -1
