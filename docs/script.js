2c35dc0cf0b9c6b4ba1049f890f4abf68daf39c7
Clement Van Truyart <me@trosa.io>
2021-01-02 10:42:04 +0100

    Added basic source central support

--- docs/script.js
+++ docs/script.js
@@ -1,9 +1,29 @@
+//
+// (c) iomonad <iomonad@riseup.net>
+// See: https://github.com/iomonad/qgis-tools
+//
+
 var mapview = L.map('mapid').setView([48.860, 2.333], 14);
 
-L.tileLayer('http://basemaps.cartocdn.com/light_all/{z}/{x}/{y}.png', {
+L.tileLayer('https://basemaps.cartocdn.com/light_all/{z}/{x}/{y}.png', {
     maxZoom: 18,
     attribution: '',
     id: 'mapbox/streets-v11',
     tileSize: 512,
     zoomOffset: -1
 }).addTo(mapview);
+
+localStorage.removeItem('centrales');
+if (localStorage.getItem('centrales') == null) {
+    let dataset_path = "https://trosa.io/qgis-tools/data/poste-source.json"
+    fetch(dataset_path)
+	.then((data) => {
+	    console.log(data);
+	    localStorage.setItem('centrales', data.json());
+	})
+	.catch ((err) => {
+	    console.log(err);
+	});
+}
+
+L.geoJSON(localStorage.getItem('centrales')).addTo(mapview);
