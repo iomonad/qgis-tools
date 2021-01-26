a4e5ee3ce6368c4e5cc449bac069753821aeaa31
Clement Van Truyart <me@trosa.io>
2021-01-26 09:28:58 +0100

    Added old legacy script

---
+++ data-fetchers/odhs_fetch.pl
@@ -0,0 +1,26 @@
+﻿use strict;
+use warnings;
+use JSON::XS;
+
+my $outdir = "Outdir";
+my $target =
+'https://opendata.hauts-de-seine.fr/explore/dataset/fr-229200506-plans-de-carrieres/download/?format=json';
+my $body = qx{curl -s $target};    # DO NOT DO THAT PLEASE
+die "Couldn't get $target" unless defined $body;
+
+my $planches = decode_json $body;
+
+mkdir $outdir;
+foreach my $planche (@$planches) {
+    my $fname = $outdir . "/" . $planche->{'fields'}->{'fichier'}->{'filename'};
+    my $path =
+"https://opendata.hauts-de-seine.fr/explore/dataset/fr-229200506-plans-de-carrieres/files/"
+      . $planche->{'fields'}->{'fichier'}->{'id'}
+      . "/download/";
+
+    if ( -e $fname ) {
+        next;
+    }
+    qx{curl --progress-bar $path --output $fname};
+    printf "DL    %s\n", $fname;
+}
